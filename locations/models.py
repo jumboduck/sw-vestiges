046776523cfe9d1834b0a5edd7a9bfe8c8f23d16
Simon Castagna <simonmerkely@Simons-MacBook-Pro-2.local>
2021-05-29 22:44:08 +0200

    Change max length of description

--- locations/models.py
+++ locations/models.py
@@ -17,7 +17,7 @@ class Location(models.Model):
     name = models.CharField(max_length=30, null=False, blank=False)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(upload_to='locations', null=True, blank=True)
-    description = models.TextField(max_length=500, null=False, blank=False)
+    description = models.TextField(max_length=2000, null=False, blank=False)
     display_rank = models.IntegerField(null=False, blank=False, default=0)
 
 
