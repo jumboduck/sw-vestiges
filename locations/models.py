2520ed0d9ba99b47db79aa3d5ccde5fb5464e9db
Simon Castagna <simonmerkely@Simons-MacBook-Pro-2.local>
2021-05-29 22:29:57 +0200

    Create locations app and models

---
+++ locations/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class Situation(models.Model):
+    is_active = models.BooleanField(null=False, default=True)
+    name = models.CharField(max_length=30, null=False, blank=False)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(upload_to='situations', null=True, blank=True)
+    description = models.TextField(max_length=500, null=False, blank=False)
+    location = models.ForeignKey(
+        'Location', null=False, blank=False, on_delete=models.CASCADE, default='void')
+    display_rank = models.IntegerField(null=False, blank=False, default=0)
+
+
+class Location(models.Model):
+    is_active = models.BooleanField(null=False, default=True)
+    name = models.CharField(max_length=30, null=False, blank=False)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(upload_to='locations', null=True, blank=True)
+    description = models.TextField(max_length=500, null=False, blank=False)
+    display_rank = models.IntegerField(null=False, blank=False, default=0)
+
+
+class LocationConnection(models.Model):
+    is_active = models.BooleanField(null=False, default=True)
+    origin = models.ForeignKey(
+        'Location', null=False, blank=False, default='void')
+    destination = models.ForeignKey(
+        'Location', null=False, blank=False, default='void')
+    is_reversible = models.BooleanField(null=False, blank=False, default=True)
