c754d1d49e0b6452768cdf273dcd3726c13bb38c
Simon Castagna <simonmerkely@Simons-MacBook-Pro-2.local>
2021-05-29 23:11:05 +0200

    Create register admin for Location and Character models

--- characters/admin.py
+++ characters/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Character
 
-# Register your models here.
+
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = (
+        'last_name',
+        'first_name',
+        'situation',
+        'is_active',
+        'is_alive',
+    )
+
+    ordering = ('last_name', 'first_name',)
+
+
+admin.site.register(Character, CharacterAdmin)
--- locations/admin.py
+++ locations/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Location, Situation, LocationConnection
 
-# Register your models here.
+
+class SituationAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'location',
+        'is_active',
+    )
+
+    ordering = ('location', 'name',)
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'is_active',
+    )
+
+    ordering = ('name',)
+
+
+class LocationConnectionAdmin(admin.ModelAdmin):
+    list_display = (
+        'origin',
+        'destination',
+        'is_active',
+    )
+
+    ordering = ('origin', 'destination')
+
+
+admin.site.register(Situation, SituationAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(LocationConnection, LocationConnectionAdmin)
