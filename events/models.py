94fcb8fee0dc7b5da2cb3f09180bc17571b368f9
Simon Castagna <sd.castagna@gmail.com>
2021-06-02 22:37:35 +0200

    Add events models

---
+++ events/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+
+class EventType(models.Model):
+    is_active = models.BooleanField(null=False, blank=False, default=True)
+    name = models.CharField(max_length=30, null=False, blank=False)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(upload_to='events', null=True, blank=True)
+    description = models.TextField(max_length=500, null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Event(models.Model):
+    is_active = models.BooleanField(null=False, blank=False, default=True)
+    author = models.ForeignKey(
+        'characters.Character', null=True, blank=True, on_delete=models.PROTECT)
+    event_type = models.ForeignKey(
+        EventType, null=False, blank=False, on_delete=models.PROTECT, default='')
+    situation_origin = models.ForeignKey('locations.Situation', null=True, blank=True,
+                                         on_delete=models.SET_NULL)
+    created = models.DateTimeField(null=False, blank=False, auto_now_add=True)
+    updated = models.DateTimeField(null=True, blank=True, auto_now=True)
+    content = models.TextField(max_length=4000, null=True, blank=True)
+
+    def __str__(self):
+        message = ''
+        date = self.created.strftime('%d/%m/%Y, %H:%M:%S')
+        if self.author:
+            message = f'({date}) {self.author}: {self.event_type}'
+        else:
+            message = f'({date}): {self.event_type}'
+        return message
+
+
+class CharacterEvent(models.Model):
+    character = models.ForeignKey(
+        'characters.Character', null=True, blank=True, on_delete=models.CASCADE)
+    event = models.ForeignKey(
+        Event, null=False, blank=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.character} / {self.event}'
