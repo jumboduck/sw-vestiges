4b6dd26cac06e3847ded2e650ce925e6c4478416
Simon Castagna <sd.castagna@gmail.com>
2021-06-05 21:18:24 +0200

    Create location view and template

--- locations/views.py
+++ locations/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Location, Situation
+from characters.models import Character
 
 
 def view_location(request, location_id):
     location = get_object_or_404(Location, pk=location_id)
     situations = Situation.objects.filter(location=location)
 
+    characters_situations = []
+
+    for situation in situations:
+        characters = Character.objects.filter(situation=situation)
+        data = {
+            'situation': situation,
+            'characters': characters
+        }
+        print(data)
+        characters_situations.append(data)
+
     context = {
         'location': location,
-        'situations': situations,
+        'characters_situations': characters_situations,
     }
     template = 'locations/location.html'
 
