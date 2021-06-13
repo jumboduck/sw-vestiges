a9324fba41ac0e8a7547ff1fdc6de496fae8ed68
Simon Castagna <sd.castagna@gmail.com>
2021-06-13 15:25:51 +0200

    Create helpers to activate or deactive characters

--- characters/helpers.py
+++ characters/helpers.py
@@ -16,3 +16,15 @@ def set_active_character(request, character_id):
     new_active_character = get_object_or_404(Character, pk=character_id)
     profile.active_character = new_active_character
     profile.save()
+
+
+def activate_character(character_id):
+    character = get_object_or_404(Character, pk=character_id)
+    character.is_active = True
+    character.save()
+
+
+def deactivate_character(character_id):
+    character = get_object_or_404(Character, pk=character_id)
+    character.is_active = False
+    character.save()
