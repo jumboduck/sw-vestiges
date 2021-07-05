fb8ef5089e0ede01d86a3f3e1caf3501e2f66e75
Simon Castagna <sd.castagna@gmail.com>
2021-07-05 22:10:28 +0200

    Ensure that only superusers can see all locations from url

--- locations/views.py
+++ locations/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.contrib.auth.decorators import login_required
 
 from .models import Location, Situation
@@ -7,23 +7,26 @@ from characters.models import Character
 
 @login_required
 def view_location(request, location_id):
-    location = get_object_or_404(Location, pk=location_id)
-    situations = Situation.objects.filter(location=location)
+    if request.user.is_superuser:
+        location = get_object_or_404(Location, pk=location_id)
+        situations = Situation.objects.filter(location=location)
 
-    characters_situations = []
+        characters_situations = []
 
-    for situation in situations:
-        characters = Character.objects.filter(situation=situation)
-        data = {
-            'situation': situation,
-            'characters': characters
+        for situation in situations:
+            characters = Character.objects.filter(situation=situation)
+            data = {
+                'situation': situation,
+                'characters': characters
+            }
+            characters_situations.append(data)
+
+        context = {
+            'location': location,
+            'characters_situations': characters_situations,
         }
-        characters_situations.append(data)
+        template = 'locations/location.html'
+        return render(request, template, context)
 
-    context = {
-        'location': location,
-        'characters_situations': characters_situations,
-    }
-    template = 'locations/location.html'
-
-    return render(request, template, context)
+    else:
+        return redirect(reverse('home'))
