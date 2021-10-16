550084c2876c89ae255cc95347bca9bff5ec2f4f
Simon Castagna <sd.castagna@gmail.com>
2021-10-16 15:55:52 +0200

    Remove unnecessary import

--- locations/helpers.py
+++ locations/helpers.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404
 from .models import Situation, LocationConnection
-from profiles.models import UserProfile
 from characters.models import Character
 
 
