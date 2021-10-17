4137bc08e4cc3db72b2118e4afaad7191b9929d3
Simon Castagna <sd.castagna@gmail.com>
2021-10-17 15:56:15 +0200

    Reset email env variables

--- sw_vestiges/settings.py
+++ sw_vestiges/settings.py
@@ -113,14 +113,13 @@ SITE_ID = 1
 # Email Management
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
     EMAIL_USE_TLS = True
-    EMAIL_HOST = os.environ.get('EMAIL_HOST')
     EMAIL_PORT = 587
+    EMAIL_HOST = os.environ.get('EMAIL_HOST')
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
