80a241a31ae4adfaf2c84c9ce850616c18c12903
Simon Castagna <sd.castagna@gmail.com>
2022-02-12 11:25:18 +0100

    Update static path in settings

--- sw_vestiges/settings.py
+++ sw_vestiges/settings.py
@@ -190,8 +190,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "frontend/build"),
 ]
