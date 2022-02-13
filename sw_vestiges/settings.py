5e302359947c29c377cb3e8a3d593eeee1f5b03d
Simon Castagna <sd.castagna@gmail.com>
2022-02-13 17:09:51 +0100

    Add bootstrap5 support to crispy forms

--- sw_vestiges/settings.py
+++ sw_vestiges/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 
     # Imports
     'crispy_forms',
+    'crispy_bootstrap5',
     'coverage',
 
     # Frontend
@@ -69,7 +70,8 @@ INSTALLED_APPS = [
     'profiles',
 ]
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
