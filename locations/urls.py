9748825e903581fe0424d027477ca566ee6167cd
Simon Castagna <sd.castagna@gmail.com>
2022-02-15 18:23:23 +0100

    Create location network map

--- locations/urls.py
+++ locations/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('<int:location_id>/', views.view_location,
          name='view_location'),
+    path('map/', views.locations_map,
+         name='locations_map'),
 ]
--- locations/views.py
+++ locations/views.py
@@ -1,8 +1,9 @@
+from characters.models import Character
+from .models import Location, Situation, LocationConnection
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.contrib.auth.decorators import login_required
-
-from .models import Location, Situation
-from characters.models import Character
+from django.contrib.admin.views.decorators import staff_member_required
+import json
 
 
 @login_required
@@ -30,3 +31,37 @@ def view_location(request, location_id):
 
     else:
         return redirect(reverse('home'))
+
+
+@staff_member_required
+def locations_map(request):
+
+    locations_json = {
+        'nodes': [],
+        'edges': [],
+    }
+    locations = Location.objects.all()
+    for location in locations:
+        if location.is_active:
+            node = {
+                'id': location.id,
+                'label': location.name
+            }
+            locations_json['nodes'].append(node)
+        else:
+            continue
+
+    location_connections = LocationConnection.objects.all()
+    for connection in location_connections:
+        if connection.is_active and connection.origin.is_active and connection.destination.is_active:
+            edge = {
+                "from": connection.origin.id,
+                "to": connection.destination.id
+            }
+            locations_json['edges'].append(edge)
+        else:
+            continue
+
+    context = {'locations_json': json.dumps(locations_json)}
+    template = 'locations/location_map.html'
+    return render(request, template, context)
