f6905f23c6c274297c4da2abeb6061d47f4db3ea
Simon Castagna <sd.castagna@gmail.com>
2022-02-17 19:46:59 +0100

    Move logs to include file

--- characters/models.py
+++ characters/models.py
@@ -20,6 +20,48 @@ class Character(models.Model):
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
-    @property
-    def location(self):
-        return self.situation.location
+#     @property
+#     def location(self):
+#         return self.situation.location
+
+
+# class AttributesField(models.Field):
+
+#     description = "A character's attributes"
+
+#     def __init__(self, power, science, dexterity, persuasion, politics, navigation, repairs, demolition, perception, force, **kwargs):
+#         self.power = power
+#         self.science = science
+#         self.dexterity = dexterity
+#         self.persuasion = persuasion
+#         self.politics = politics
+#         self.navigation = navigation
+#         self.repairs = repairs
+#         self.demolition = demolition
+#         self.perception = perception
+#         self.force = force
+
+#     def deconstruct(self):
+#         name, path, args, kwargs = super().deconstruct()
+#         kwargs['power'] = self.power
+#         kwargs['science'] = self.science
+#         kwargs['dexterity'] = self.dexterity
+#         kwargs['persuasion'] = self.persuasion
+#         kwargs['politics'] = self.politics
+#         kwargs['navigation'] = self.navigation
+#         kwargs['repairs'] = self.repairs
+#         kwargs['demolition'] = self.demolition
+#         kwargs['perception'] = self.perception
+#         kwargs['force'] = self.force
+#         return name, path, args, kwargs
+
+    # power = models.PositiveIntegerField(null=False, blank=False)
+    # science = models.PositiveIntegerField(null=False, blank=False)
+    # dexterity = models.PositiveIntegerField(null=False, blank=False)
+    # persuasion = models.PositiveIntegerField(null=False, blank=False)
+    # politics = models.PositiveIntegerField(null=False, blank=False)
+    # navigation = models.PositiveIntegerField(null=False, blank=False)
+    # repairs = models.PositiveIntegerField(null=False, blank=False)
+    # demolition = models.PositiveIntegerField(null=False, blank=False)
+    # perception = models.PositiveIntegerField(null=False, blank=False)
+    # force = models.PositiveIntegerField(null=False, blank=False)
