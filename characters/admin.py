7a1dddf0c4aab0531423aa5d774d95cae63d3b3e
Simon Castagna <sd.castagna@gmail.com>
2022-02-21 19:51:02 +0100

    Ensure inline fields in admin save when default is set

--- characters/admin.py
+++ characters/admin.py
@@ -1,50 +1,80 @@
 from django.contrib import admin
+from django.forms.models import ModelForm
 from .models import Character, Power, Science, Dexterity, Persuasion, Politics, Navigation, Piloting, \
     Repair, Demolition, Perception, Force
 
 
+class AlwaysChangedModelForm(ModelForm):
+    def has_changed(self):
+        """ Should returns True if data differs from initial. 
+        By always returning true even unchanged inlines will get validated and saved."""
+        return True
+
+
 class PowerAttrInline(admin.TabularInline):
     model = Power
+    extra = 1
+    form = AlwaysChangedModelForm
 
 
 class ScienceAttrInline(admin.TabularInline):
     model = Science
+    extra = 1
+    form = AlwaysChangedModelForm
 
 
 class DexterityAttrInline(admin.TabularInline):
     model = Dexterity
+    extra = 1
+    form = AlwaysChangedModelForm
 
 
 class PersuasionAttrInline(admin.TabularInline):
     model = Persuasion
+    extra = 1
+    form = AlwaysChangedModelForm
 
 
 class PoliticsAttrInline(admin.TabularInline):
     model = Politics
+    extra = 1
+    form = AlwaysChangedModelForm
 
 
 class NavigationAttrInline(admin.TabularInline):
     model = Navigation
+    extra = 1
+    form = AlwaysChangedModelForm
 
 
 class PilotingAttrInline(admin.TabularInline):
     model = Piloting
+    extra = 1
+    form = AlwaysChangedModelForm
 
 
 class RepairAttrInline(admin.TabularInline):
     model = Repair
+    extra = 1
+    form = AlwaysChangedModelForm
 
 
 class DemolitionAttrInline(admin.TabularInline):
     model = Demolition
+    extra = 1
+    form = AlwaysChangedModelForm
 
 
 class PerceptionAttrInline(admin.TabularInline):
     model = Perception
+    extra = 1
+    form = AlwaysChangedModelForm
 
 
 class ForceAttrInline(admin.TabularInline):
     model = Force
+    extra = 1
+    form = AlwaysChangedModelForm
 
 
 class CharacterAdmin(admin.ModelAdmin):
